a7ad40abf1c4dc727de2f69d72d2df55e08dcaea
Aniket <aniketupatel1@gmail.com>
2021-08-15 11:59:18 -0400

    first header commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Map from "./Components/Map/Map";
 const App = () => {
   return (
     <React.Fragment>
+      <CssBaseline/>
       <Header/>
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
