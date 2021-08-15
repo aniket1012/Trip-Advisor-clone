77c999f02509dc00aa63944c58405634c80cd792
Aniket <aniketupatel1@gmail.com>
2021-08-15 11:57:10 -0400

    created app skelaton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { 
+  CssBaseline,
+  Grid,
+
+ } from "@material-ui/core";
+
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+
 
 const App = () => {
   return (
-    <div className="App">
-      Hello World 
+    <React.Fragment>
+      <Header/>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List/>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map/>
+        </Grid>
+      </Grid>
     
-    </div>
+    </React.Fragment>
   );
 }
 
