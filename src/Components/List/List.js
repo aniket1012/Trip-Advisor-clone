56ebbc43f17e31a093ca076f5f08b9d2048678ef
Aniket <aniketupatel1@gmail.com>
2021-08-16 15:27:43 -0400

    added import files and styles

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,6 +1,20 @@
 import React from 'react'
+import { 
+    CircularProgress,
+    Grid,
+    Typography,
+    InputLabel,
+    MenuItem,
+    FormControl,
+    Select 
+} from '@material-ui/core'
+
+import useStyles from './styles'
+
 
 const List = () => {
+
+    const classes = useStyles()
     return (
         <h1>
             List
