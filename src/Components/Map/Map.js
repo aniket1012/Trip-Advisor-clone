5bb5ecf7e00e5728cb9454960d32c0d469f8aec9
Aniket <aniketupatel1@gmail.com>
2021-08-16 14:23:20 -0400

    imported files and dependencies

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import googleMapReact from 'google-map-react'
+import { 
+    Paper,
+    Typography,
+    useMediaQuery,
+} from '@material-ui/core'
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from '@material-ui/lab'
 
 const Map = () => {
     return (
