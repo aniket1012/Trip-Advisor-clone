f123c084dab9aa902c40c53813dbeef11d497a6d
Aniket <aniketupatel1@gmail.com>
2021-08-17 11:57:44 -0400

    added imports and styles to Plave Details to being building Card to display info

--- src/Components/PlaceDetails/PlaceDetails.js
+++ src/Components/PlaceDetails/PlaceDetails.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import { 
+    Box,
+    Typography,
+    Button,
+    Card,
+    CardContent,
+    CardMedia,
+    CardActions,
+    Chip
+ } from '@material-ui/core';
+ import LocationonIcon from '@material-ui/icons/LocationOn'
+ import PhoneIcon from '@material-ui/icons/Phone'
+ import { Rating } from '@material-ui/lab/Rating';
 
+
+ import useStyles from './styles'
+
+ 
 const PlaceDetails = ({place}) => {
+
+    console.log(place);
     return (
         <h1>
             {place.name}
