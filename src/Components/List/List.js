28fc76834a9622c38a47bc57b2b07af687cfdccd
Aniket <aniketupatel1@gmail.com>
2021-08-20 23:04:02 -0400

    fixing bug from uncaught promise

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,87 +1,174 @@
-import React, {useState, useEffect, createRef} from 'react'
-import { 
-    CircularProgress,
-    Grid,
-    Typography,
-    InputLabel,
-    MenuItem,
-    FormControl,
-    Select 
-} from '@material-ui/core'
+// import React, {useState, useEffect, createRef} from 'react'
+// import { 
+//     CircularProgress,
+//     Grid,
+//     Typography,
+//     InputLabel,
+//     MenuItem,
+//     FormControl,
+//     Select 
+// } from '@material-ui/core'
 
-import PlaceDetails from '../PlaceDetails/PlaceDetails'
-import useStyles from './styles'
+// import PlaceDetails from '../PlaceDetails/PlaceDetails'
+// import useStyles from './styles'
 
 
-const List = ({places, childClicked, isLoading, type, setType, rating, setRating}) => {
+// const List = ({places, childClicked, isLoading, type, setType, rating, setRating}) => {
 
-    const [elRefs, setElRefs] = useState([])
-    const classes = useStyles()
+//     const [elRefs, setElRefs] = useState([])
+//     const classes = useStyles()
     
 
 
-    // useEffect(() => {
-    //     const refs = Array(places?.length)?.fill()?.map(( _, i)=> refs[i] || createRef())
+//     // useEffect(() => {
+//     //     const refs = Array(places?.length)?.fill()?.map(( _, i)=> refs[i] || createRef())
         
-    //     setElRefs(refs)
-    // }, [places])
+//     //     setElRefs(refs)
+//     // }, [places])
 
-    useEffect(() => {
-      setElRefs((refs) =>
-        Array(places.length)
-          .fill()
-          .map((_, i) => refs[i] || createRef())
-      );
-    }, [places]);
+//     useEffect(() => {
+//       setElRefs((refs) =>
+//         Array(places.length)
+//           .fill()
+//           .map((_, i) => refs[i] || createRef())
+//       );
+//     }, [places]);
 
 
-    return (
-      <div className={classes.container}>
-        <Typography variant="h4">
-          {" "}
-          Restaurants, Hotels, & Attractions around you
-        </Typography>
-        {isLoading ? (
-          <div className={classes.loading}>
-            <CircularProgress size="5rem" />
-          </div>
-        ) : (
-          <React.Fragment>
-            <FormControl className={classes.formControl}>
-              <InputLabel>Type</InputLabel>
-              <Select value={type} onChange={(e) => setType(e.target.value)}>
-                <MenuItem value="restaurants">Restaurants</MenuItem>
-                <MenuItem value="hotels">Hotels</MenuItem>
-                <MenuItem value="attractions">Attractions</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl className={classes.formControl}>
-              <InputLabel>Rating</InputLabel>
-              <Select
-                value={rating}
-                onChange={(e) => setRating(e.target.value)}
-              >
-                <MenuItem value={0}>All</MenuItem>
-                <MenuItem value={3}>Above 3.0</MenuItem>
-                <MenuItem value={4}>Above 4.0</MenuItem>
-                <MenuItem value={4.5}>Above 4.5</MenuItem>
-              </Select>
-            </FormControl>
-            <Grid container spacing={3} className={classes.list}>
-              {places.map((place, i) => (
-                <Grid ref={elRefs[i]} item key={i} xs={12}>
-                  <PlaceDetails
-                    selected={Number(childClicked) === i}
-                    refProp={elRefs[i]}
-                    place={place}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </React.Fragment>
-        )}
-      </div>
+//     return (
+//       <div className={classes.container}>
+//         <Typography variant="h4">
+//           {" "}
+//           Restaurants, Hotels, & Attractions around you
+//         </Typography>
+//         {isLoading ? (
+//           <div className={classes.loading}>
+//             <CircularProgress size="5rem" />
+//           </div>
+//         ) : (
+//           <React.Fragment>
+//             <FormControl className={classes.formControl}>
+//               <InputLabel>Type</InputLabel>
+//               <Select value={type} onChange={(e) => setType(e.target.value)}>
+//                 <MenuItem value="restaurants">Restaurants</MenuItem>
+//                 <MenuItem value="hotels">Hotels</MenuItem>
+//                 <MenuItem value="attractions">Attractions</MenuItem>
+//               </Select>
+//             </FormControl>
+//             <FormControl className={classes.formControl}>
+//               <InputLabel>Rating</InputLabel>
+//               <Select
+//                 value={rating}
+//                 onChange={(e) => setRating(e.target.value)}
+//               >
+//                 <MenuItem value={0}>All</MenuItem>
+//                 <MenuItem value={3}>Above 3.0</MenuItem>
+//                 <MenuItem value={4}>Above 4.0</MenuItem>
+//                 <MenuItem value={4.5}>Above 4.5</MenuItem>
+//               </Select>
+//             </FormControl>
+//             <Grid container spacing={3} className={classes.list}>
+//               {places.map((place, i) => (
+//                 <Grid ref={elRefs[i]} item key={i} xs={12}>
+//                   <PlaceDetails
+//                     selected={Number(childClicked) === i}
+//                     refProp={elRefs[i]}
+//                     place={place}
+//                   />
+//                 </Grid>
+//               ))}
+//             </Grid>
+//           </React.Fragment>
+//         )}
+//       </div>
+//     );
+// }
+
+// export default List
+
+import React, { useState, useEffect, createRef } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails";
+import useStyles from "./styles.js";
+
+const List = ({
+  places,
+  type,
+  setType,
+  rating,
+  setRating,
+  childClicked,
+  isLoading,
+}) => {
+  const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
+
+  useEffect(() => {
+    setElRefs((refs) =>
+      Array(places.length)
+        .fill()
+        .map((_, i) => refs[i] || createRef())
     );
-}
+  }, [places]);
 
-export default List
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select
+              id="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select
+              id="rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+            >
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          <Grid container spacing={3} className={classes.list}>
+            {places?.map((place, i) => (
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <PlaceDetails
+                  selected={Number(childClicked) === i}
+                  refProp={elRefs[i]}
+                  place={place}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default List;
